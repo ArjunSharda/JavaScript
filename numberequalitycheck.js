8c795c83b881ce91ae1f9b75e9910c7296103dbf
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2022-05-09 10:08:23 -0500

    Create numberequalitycheck.js
    Originally a test I wanted to try for two variables, but I assume you can call the function with multiple variables and it works

---
+++ numberequalitycheck.js
@@ -0,0 +1,12 @@
+let num1 = 1000
+let num2 = 1000
+let num3 = 1000
+
+function CheckNumbers(num1, num2) {
+  if (num1 == num2) {
+    console.log("The variables match! Wohoo!");
+  } else {
+    console.log("No, sorry they don't match");
+  }
+}
+CheckNumbers(num1, num2, num3)
