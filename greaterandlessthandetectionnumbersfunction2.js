d874dd4e4cec835f803a0cb76f10f4217ad937a0
ajsharda17 <77706434+ajsharda17@users.noreply.github.com>
2022-05-17 13:56:47 -0500

    Create greaterandlessthandetectionnumbersfunction2.js

---
+++ greaterandlessthandetectionnumbersfunction2.js
@@ -0,0 +1,19 @@
+function test () {
+  number = 100
+  if (number < 10) {
+    console.log("DETECTION: Number is under 10");
+  }
+  if (number < 50) {
+    console.log("DETECTION: Number is under 50");
+  }
+  if (number < 100) {
+    console.log("DETECTION: Number is under 100");
+  }
+  if (number < 1000) {
+    console.log("DETECTION: Number is under 1,000");
+  }
+  if (number > 1000) {
+    console.log("DETECTION: Number is over 1,000");
+  }
+}
+test()
